cbf6fe0faf56540f8d7c0a278450c493707547e1
zhaokaiqiang <kaiqiang.zh@gmail.com>
2023-07-20 22:04:45 +0800

    feat: 修改 about

--- pages/about.vue
+++ pages/about.vue
@@ -5,35 +5,9 @@
     </div>
     <div class="m-5">
       <div class="py-5">
-        <p class="text-xl font-semibold mb-5">探报是什么?</p>
+        <p class="text-xl font-semibold mb-5">LinkBlog 是什么?</p>
         <div class="text-base text-gray-600">
-          <p>探报，是一个每周推荐互联网优质内容的网站</p>
-          <br>
-          <p>任何人也都可以向「探报」推荐你自己或你喜欢的内容</p>
-          <p>1. 你可以将你写在飞书、语雀、notion 等笔记软件的内容通过链接的形式分享</p>
-          <p>2. 你可以将你发布在公众号、博客、播客、视频网站等平台的内容通过链接的形式分享</p>
-          <p>3. 你可以将你喜欢的内容通过链接的形式分享</p>
-        </div>
-      </div>
-      <div class="py-5">
-        <p class="text-xl font-semibold mb-5">推荐的频次是什么?</p>
-        <div class="text-base text-gray-600">
-          <p>前期会在每周一、三、五推荐一个内容</p>
-        </div>
-      </div>
-      <div class="py-5">
-        <p class="text-xl font-semibold mb-5">可以推荐什么类型的内容?</p>
-        <div class="text-base text-gray-600">
-          <p>内容类型不限制，可以是文章、视频、播客和产品等, 但需要至少符合下面两个条件中的一个：</p>
-          <p>1. 能呈现创作者「探」索精神的内容</p>
-          <p>2. 能帮助读者「探」索更大世界的内容</p>
-        </div>
-      </div>
-      <div class="py-5">
-        <p class="text-xl font-semibold mb-5">推荐内容能获得什么?</p>
-        <div class="text-base text-gray-600">
-          <p>你可以在推荐表单内填写你的昵称和一个社交媒体账号链接</p>
-          <p>在「探报」页面内，用户可以直接点击你的昵称，跳转到你的社交媒体页面, 从而获得关注度</p>
+          <p>探报，是一个收集和展示互联网优质内容的网站 ...</p>
         </div>
       </div>
     </div>
